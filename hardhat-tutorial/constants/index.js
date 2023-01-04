282efd018d37f27f060708a7c1252a4469193f72
Muhammad Adnan <adnanbest1122@gmail.com>
2023-01-04 16:50:52 +0500

    ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB460Df55e53E312F9f54B54CAAFb8129adec0dDC";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
